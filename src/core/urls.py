e6deee50a5304cb59e6afb6ca962021fe10857d0
Rem Lampa <rem@remlampa.com>
2020-02-13 23:29:47 +0800

    Implement auth :sparkles:

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import HomeView, ItemDetailView, add_to_cart, checkout
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('checkout/', checkout, name='checkout'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', add_to_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/', add_to_cart, name='remove-from-cart'),
+    path(r'^accounts/', include('allauth.urls'))
 ]
