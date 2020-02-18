44e1ac2c34250d2a2dd713c93a168bff0d33ee33
Rem Lampa <rem@remlampa.com>
2020-02-18 21:51:43 +0800

    Fix outdated url pattern :wrench:

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import include, path
 
 from .views import HomeView, ItemDetailView, add_to_cart, checkout
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', add_to_cart, name='remove-from-cart'),
-    path(r'^accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls'))
 ]
