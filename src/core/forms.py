56aa20b381ef7e6464148a827f5ae86a69bd64fb
Rem Lampa <rem@remlampa.com>
2020-02-25 20:15:53 +0800

    Implement Checkout page :sparkles:

---
+++ src/core/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+PAYMENT_CHOICES = (('S', 'Stripe'), ('P', 'Paypal'))
+
+
+class CheckoutForm(forms.Form):
+    street_address = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "placeholder": "1234 Main St.",
+            "class": "form-control"
+        }))
+    apartment_address = forms.CharField(required=False,
+                                        widget=forms.TextInput(attrs={
+                                            "placeholder":
+                                            "Apartment or suite",
+                                            "class":
+                                            "form-control"
+                                        }, ))
+    country = CountryField(
+        blank_label="Select Country", ).formfield(widget=CountrySelectWidget(
+            attrs={"class": "custom-select d-block w-100"}), )
+    zip = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "form-control"}))
+    same_shipping_address = forms.BooleanField(
+        widget=forms.CheckboxInput,
+        required=False,
+    )
+    save_info = forms.BooleanField(widget=forms.CheckboxInput, required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect(),
+        choices=PAYMENT_CHOICES,
+    )
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 
-from .views import (HomeView, ItemDetailView, OrderSummaryView, add_to_cart,
-                    checkout, remove_from_cart, remove_single_item_from_cart)
+from .views import (CheckoutView, HomeView, ItemDetailView, OrderSummaryView,
+                    add_to_cart, remove_from_cart,
+                    remove_single_item_from_cart)
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('checkout/', checkout, name='checkout'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('cart/', OrderSummaryView.as_view(), name='cart'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
